f20f225f1eaf01dd554ab3c21c3dbd95bf40542c
network2501 <pete@network2501.com>
2018-03-10 22:32:52 +0800

    Added Gruvbox Dark Medium theme with orange cursor

---
+++ themes/Gruvbox Dark Medium.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#282828", "#cc241d", "#98971a", "#d79921", "#458588", "#b16286", "#689d6a", "#d65d0e", "#928374", "#fb4934", "#b8bb26", "#fabd2f", "#83a598", "#d3869b", "#8ec07c", "#fe8019"]);
+t.prefs_.set('foreground-color', "#ebdbb2");
+t.prefs_.set('background-color', "#282828");
+t.prefs_.set('cursor-color', 'rgba(254,128,25,0.5)');
